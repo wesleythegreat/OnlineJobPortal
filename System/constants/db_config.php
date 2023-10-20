f50b7339ca5ca4be6d51baac085b67a7ef801dad
wesley <gameking232nd@gmail.com>
2023-10-20 12:24:25 +0800

    bentong

---
+++ System/constants/db_config.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "onlinejobportal";
+$username = "root";
+$password = "";
+$dbname = "job_portal";
+?>
---
+++ System/constants/settings.php
@@ -0,0 +1,14 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = 'asia';
+$smtp_host = 'wesley';
+$smtp_user = 'root';
+$smtp_pass = '123';
+
+$contact_mail = '';
+
+$fb  = '#';
+$tw = '#';
+$ig = '#';
+?>
