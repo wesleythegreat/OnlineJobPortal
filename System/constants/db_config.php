53a66658d87ab859c561d4784bf544dfa44cf43a
wesley <gameking232nd@gmail.com>
2023-10-20 12:27:14 +0800

    replacing username

--- System/constants/db_config.php
+++ System/constants/db_config.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "localhost"; // Update this with your database server's hostname or IP address
-$username = "root"; // Update this with your database username
+$username = "127.0.0.1"; // Update this with your database username
 $password = ""; // Update this with your database password
 $dbname = "job_portal"; // Update this with your database name
 ?>
